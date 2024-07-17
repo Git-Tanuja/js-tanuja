604dc7fb3d832f8b595c9c8c2f3223bd35fde880
Git-Tanuja <tanujashete97@gmail.com>
2024-07-17 08:32:41 +0000

    config done

---
+++ js_basics/07_number.js
@@ -0,0 +1,37 @@
+//NUmbers
+/*
+let score = 100;
+console.log(score)      //100
+
+let balance = new Number(100)
+console.log(balance)        //[Number: 100]
+
+console.log(balance.toString().length)  // 3
+console.log(balance.toFixed(2))         // 100.00
+console.log(balance.toExponential(3))   //1.000e+2
+console.log(balance.toLocaleString())   //100
+
+let myPrecision = 12.34566
+console.log(myPrecision.toPrecision(3))     //12.3
+*/
+
+// ++++++++++++++ Math +++++++++++++++++
+//Math Properties
+// console.log(Math)               // Object [Math] {}
+// console.log(Math.PI)            // 3.141592653589793
+// console.log(Math.E)             // 2.718281828459045
+// console.log(Math.SQRT2)         // 1.4142135623730951
+
+
+
+//Math Method
+// console.log(Math.abs(-5.5))     // 5.5
+// console.log(Math.round(-5.5))   // -5
+// console.log(Math.ceil(-5.5))    // -5
+// console.log(Math.floor(-5.5))   // -6
+// console.log(Math.trunc(-5.5))   // -5
+console.log(Math.pow(8, 2));        // 64
+console.log(Math.sqrt(64));         // 8
+console.log(Math.min(0, 150, 30, 20, -8, -200));    // -200
+console.log(Math.max(0, 150, 30, 20, -8, -200));    // 150
+console.log(Math.random());             // 0.603438795465
