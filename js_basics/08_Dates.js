86e55f7b794aed8a6d4e9774a377ab89497e2250
Git-Tanuja <tanujashete97@gmail.com>
2024-07-17 13:03:22 +0000

    config done

---
+++ js_basics/08_Dates.js
@@ -0,0 +1,16 @@
+//Inbuilt Date
+// let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+
+
+// //Created Date
+// let myCreatedDate = new Date(2023,0,23)
+// console.log(myCreatedDate.toLocaleString())
+
+const A =[10,11,12,13,14,15,16];
+A.push(100)
+console.log(A);
+// console.log(B);
+
